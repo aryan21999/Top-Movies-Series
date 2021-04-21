639a5e738943d8de7d7a12ba4924d79e00bdbb31
Aryan <test@test.com>
2021-04-21 16:26:16 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,27 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Card(props){
+  return (
   <>
       <div className="cards">
       <div className="card">
-        <img src="" alt="myPic" className="card__img" />
+        <img src={props.imgsrc} alt="myPic" className="card__img" />
         <div className="card__info">
-          <span className="card__category">  </span>
-          <h3 className="card__title"> Stranger Things </h3>
-          <a href="" target="_blank">
+          <span className="card__category">{props.title}</span>
+          <h3 className="card__title">{props.sname}</h3>
+          <a href={props.link} target="_blank">
             <button> Watch Now </button>
           </a>
         </div>
       </div>
       </div>
     </>
-    
+  );
 }
 
 ReactDOM.render(
     <>
      <Card 
-       imgsrc=""
+       imgsrc="https://images-na.ssl-images-amazon.com/images/I/61gi3wqRKFL.jpg"
        title=" A Netflix Original Series "
        sname=" Stranger Things "
        link="https://www.netflix.com/in/title/80057281"
