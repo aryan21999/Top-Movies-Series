db0f9445d48eb76414b47b8aeace39e54d605ece
Aryan <test@test.com>
2021-04-21 16:18:41 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Card(props){
+  <>
+      <div className="cards">
+      <div className="card">
+        <img src="" alt="myPic" className="card__img" />
+        <div className="card__info">
+          <span className="card__category">  </span>
+          <h3 className="card__title"> Stranger Things </h3>
+          <a href="" target="_blank">
+            <button> Watch Now </button>
+          </a>
+        </div>
+      </div>
+      </div>
+    </>
+    
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <>
+     <Card 
+       imgsrc=""
+       title=" A Netflix Original Series "
+       sname=" Stranger Things "
+       link="https://www.netflix.com/in/title/80057281"
+     />
+     <Card />
+    </>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
