deb1340f08872f5158c4a329ff4a9141a604e417
Aryan <test@test.com>
2021-04-22 13:15:31 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -28,6 +28,18 @@ ReactDOM.render(
        sname={Sdata[2].sname}
        link={Sdata[2].link}
      />
+     <Card 
+       imgsrc={Sdata[3].imgsrc}
+       title={Sdata[3].title}
+       sname={Sdata[3].sname}
+       link={Sdata[3].link}
+     />
+     <Card 
+       imgsrc={Sdata[4].imgsrc}
+       title={Sdata[4].title}
+       sname={Sdata[4].sname}
+       link={Sdata[4].link}
+     />
      
     </>,
   document.getElementById('root')
