085e789bb5e90c5e8a88e6df96b9de1ccdbba8bc
Aryan <test@test.com>
2021-05-23 19:44:54 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,21 +5,23 @@ import "./index.css";
 import Sdata from './Sdata';
 
 function ncard(val,index){
-  return (
-    <Card 
-    imgsrc={val.imgsrc}
-    title={val.title}
-    sname={val.sname}
-    link={val.link}
-  />
-  )
+  
 }
 
 ReactDOM.render(
     <>
     <h1 className='heading_style'> List of top 5 Netflix Series in 2020 </h1>
 
-    {Sdata.map(ncard)}
+    {Sdata.map((val, index) => {
+      return (
+    <Card 
+    imgsrc={val.imgsrc}
+    title={val.title}
+    sname={val.sname}
+    link={val.link}
+  />
+  );
+    })}
      
     </>,
   document.getElementById('root')
